38662816a7fb8716befa2cf07c1a530a550a337c
Alex-Berezov <aleber197@gmail.com>
2023-07-29 13:25:08 +0400

    Dug fixes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,8 @@ import * as Styled from './styles'
 import { Header } from '../../components/Header'
 import { OftenOrdered } from './components/OftenOrdered'
 import { ProductsBlock } from './components/ProductsBlock'
-import { productsAPI } from '../../services/ProductsService'
 
 const Home: FC = () => {
-  const { data } = productsAPI.useFetchAllProductsQuery('')
-
   return (
     <Styled.Root>
       <Header />
